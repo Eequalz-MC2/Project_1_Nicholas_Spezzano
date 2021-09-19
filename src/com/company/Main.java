9fee333ef753ad1beb3e3e7a12c18dd3524b3a6c
Eequalz-MC2 <89940280+Eequalz-MC2@users.noreply.github.com>
2021-09-19 18:53:29 -0400

    ROUGH DRAFT PROJECT 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,27 +6,74 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        var productfile= "Products.txt";
+        var productfile = "Products.txt";
         var productsfile = Paths.get(productfile);
         var nameof_product = new ArrayList<String>();
         var costof_product = new ArrayList<Float>();
         var numberof_products = new ArrayList<Integer>();
         var read_txt = Files.readAllLines(productsfile);
 
-        System.out.println("Hey" +read_txt);
+        System.out.println("Hey" + read_txt);
 
-        for (var lookupproductloop : read_txt ) {
+        for (var lookupproductloop : read_txt) {
             var seperate_txt = lookupproductloop.split(",");
-            System.out.println(lookupproductloop);
+
+            nameof_product.add(seperate_txt[0]); //what does this do again?
+            costof_product.add(Float.parseFloat(seperate_txt[1]));
+            numberof_products.add(Integer.parseInt(seperate_txt[2]));
+
         }
 
-            //System.out.println("Heres the list" );
+        var keepGoing = true;
+        var reader = new Scanner(System.in);
+
+        while (keepGoing) {
+
+
+            System.out.print("Enter a product name to search, or type 'END' to quit: ");
+
+            var userResponse = reader.nextLine();
+
+            if (userResponse.equals("END")) {
+
+                keepGoing = false;
+
+            } else {
+
+                var indexOfProduct = nameof_product.indexOf(userResponse);
+
+                if (indexOfProduct == -1) {
+
+                    System.out.print("The product is not in stock\n");
+
+                } else {
+
+                    var name = nameof_product.get(indexOfProduct);
+                    var price = costof_product.get(indexOfProduct);
+                    var quantity = numberof_products.get(indexOfProduct);
+
+
+                    System.out.print(name + " is in stock! We have " + quantity + ". The price of each is $" + price + "\n");
+
+                }
+
+
+            }
+
+
         }
+
+        System.out.print("\n\nAll Done");
+
+
         // So far we did
-	// write your code here
+        // write your code here
     }
+
+
 }
